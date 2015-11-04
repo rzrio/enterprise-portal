c61a9df327ae75a8d202a33ff1e86e960cee2be2
Jawad <hello@rzr.io>
2015-11-04 14:54:38 +0000

    positioning on the main page

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -79,14 +79,15 @@ $this->title = "rzr.io"
                                         <script src="http://d3js.org/topojson.v1.min.js"></script>
                                         <!-- I recommend you host this file on your own, since this will change without warning -->
                                         <script src="http://datamaps.github.io/scripts/datamaps.world.min.js?v=1"></script>
-                                        <div id="container1" style="position: absolute; top: 200px; left: 80px; height: 800px; margin: 0 auto; z-index: 60000"></div>
+                                        <div id="container1" style="position: absolute; top: 20px; right: 80px; height: 800px; margin: 0 auto; z-index: 60000"></div>
 
 
                                         <script>
                                             //basic map config with custom fills, mercator projection
                                             var map = new Datamap({
                                                 scope: 'world',
-                                                element: document.getElementById('container1')
+                                                element: document.getElementById('container1'),
+                                                height: 1000
                                             })
 
 
