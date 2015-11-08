dbc54f8f11b3d358ea86ce2d5ddd1ce733a2820c
Jawad <hello@rzr.io>
2015-11-08 12:39:42 +0000

    scaling map up to 80%

--- frontend/views/site/worldmap.php
+++ frontend/views/site/worldmap.php
@@ -11,7 +11,7 @@ $this->title = "World Map"
 <script src="//cdnjs.cloudflare.com/ajax/libs/d3/3.5.3/d3.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/topojson/1.6.9/topojson.min.js"></script>
 <script src="/js/datamaps.world.min.js"></script>
-<div id="rzr-world-map" style="position: relative; width: 800px; height: 600px;"></div>
+<div id="rzr-world-map" style="position: relative; width: 80%; height: 80%;"></div>
 <script>
     var map = new Datamap({element: document.getElementById('rzr-world-map')});
 </script>
