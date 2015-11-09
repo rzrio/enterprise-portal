50db747b1fb47e120af7dfa67009082bebfec503
Jawad <hello@rzr.io>
2015-11-09 14:22:54 +0000

    adding pills for main menu in site/index

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -30,6 +30,35 @@ $this->title = "rzr.io"
 
         <h2> Welcome <?= Yii::$app->user->identity->username ?>, you're already logged in.</h2>
 
+        <div class="container">
+            <h2>Main Menu</h2>
+            <ul class="nav nav-pills">
+                <li class="active"><a data-toggle="pill" href="#home">Home</a></li>
+                <li><a data-toggle="pill" href="#partners">Partners</a></li>
+                <li><a data-toggle="pill" href="#contracts">Contracts</a></li>
+                <li><a data-toggle="pill" href="#sales">Sales</a></li>
+            </ul>
+
+            <div class="tab-content">
+                <div id="home" class="tab-pane fade in active">
+                    <h3>HOME</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <div id="partners" class="tab-pane fade">
+                    <h3>Partners</h3>
+                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+                <div id="contracts" class="tab-pane fade">
+                    <h3>Contracts</h3>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                </div>
+                <div id="sales" class="tab-pane fade">
+                    <h3>Sales</h3>
+                    <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                </div>
+            </div>
+        </div>
+
     </div>
 
 
