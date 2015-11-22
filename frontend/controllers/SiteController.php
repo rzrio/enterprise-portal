0d86a6c9649178bf86ec9b70381c0eae55f31aef
Jawad <hello@rzr.io>
2015-11-22 17:03:21 +0000

    access control filters on the site controller

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -2,6 +2,7 @@
 namespace frontend\controllers;
 
 use Yii;
+use yii\filters\AccessControl;
 use frontend\models\ContactForm;
 use yii\web\Controller;
 use common\models\User;
@@ -17,6 +18,27 @@ class SiteController extends Controller
     /**
      * @inheritdoc
      */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['login', 'logout', 'signup'],
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'actions' => ['login', 'signup'],
+                        'roles' => ['?'],
+                    ],
+                    [
+                        'allow' => true,
+                        'actions' => ['logout'],
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ];
+    }
     public function actions()
     {
         return [
