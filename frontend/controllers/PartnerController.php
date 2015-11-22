32607b17dea3940faa34b85d9c38d4b53cbe9563
Jawad <hello@rzr.io>
2015-11-22 17:11:08 +0000

    access control filters on the partner controller

--- frontend/controllers/PartnerController.php
+++ frontend/controllers/PartnerController.php
@@ -8,6 +8,7 @@ use app\models\PartnerSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
 
 /**
  * PartnerController implements the CRUD actions for Partner model.
