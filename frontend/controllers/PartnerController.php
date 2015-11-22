82dda047cc6937c14c032caf021bf73a7ca23c3e
Jawad <hello@rzr.io>
2015-11-22 17:08:21 +0000

    access control filters on the partner controller

--- frontend/controllers/PartnerController.php
+++ frontend/controllers/PartnerController.php
@@ -23,6 +23,22 @@ class PartnerController extends Controller
                     'delete' => ['post'],
                 ],
             ],
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['login', 'logout', 'signup'],
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'actions' => ['login', 'signup'],
+                        'roles' => ['?'],
+                    ],
+                    [
+                        'allow' => true,
+                        'actions' => ['logout'],
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
         ];
     }
 
