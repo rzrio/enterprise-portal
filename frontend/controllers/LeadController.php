9122900f9c928bfea1e5b48b5b7f6c546367d0c6
Jawad <hello@rzr.io>
2015-11-23 13:32:14 +0000

    add access control filter to lead controller - only logged in users can see this.

--- frontend/controllers/LeadController.php
+++ frontend/controllers/LeadController.php
@@ -8,6 +8,7 @@ use app\models\LeadSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
 
 /**
  * LeadController implements the CRUD actions for Lead model.
