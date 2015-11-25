86c552d79a0bc9373dc1595337f41c29fb9900a1
Jawad <hello@rzr.io>
2015-11-25 20:08:31 +0000

    making signin form thinner

--- frontend/modules/user/views/sign-in/login.php
+++ frontend/modules/user/views/sign-in/login.php
@@ -30,8 +30,9 @@ $this->params['breadcrumbs'][] = $this->title;
 </div>
 
     <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-
 <script src="js/splashindex.js"></script>
+
+<script src="js/index.js"></script>
 
 
 
