68e783eae044a31d725df5a7d184ed7bb1d8a100
Jawad <hello@rzr.io>
2015-11-25 19:53:49 +0000

    making signin form thinner

--- frontend/modules/user/views/sign-in/login.php
+++ frontend/modules/user/views/sign-in/login.php
@@ -13,7 +13,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?php echo Html::encode($this->title) ?></h1>
 
 
-        <div class="container" style="width: 20%;bottom: 10px;">
+        <div class="container" style="position: absolute; width: 20%; bottom: 10px; left: 10px;">
             <?php $form = ActiveForm::begin(['id' => 'login-form','class' => 'form']); ?>
             <?php echo $form->field($model, 'identity') ?>
             <?php echo $form->field($model, 'password')->passwordInput() ?>
